99ed6690b9dde5946073f585540a4fab6b3bbb21
Patseroni <patrik.bustad@lerniastudent.se>
2025-03-30 21:25:30 +0200

    fix comments and duplicate tests in the test-file

--- function.test.js
+++ function.test.js
@@ -3,8 +3,8 @@
 // Test där alla bokstäver är correct.
 // Testar en ord där alla bokstäver är incorrect.
 // Test med bokstäver där det är blandat med correct, misplaced och incorrect.
-// Testar ord som har fler av samma bokstav.
-// Testar ord som blandar stora och små bostäver i både query-ordet och gissningsordet.
+// Tester av ord som har fler av samma bokstav.
+// Test av ord som blandar stora och små bostäver i både query-ordet och gissningsordet.
 
 import { checkGuess } from './function.js';
 describe('checkGuess()', () => {
@@ -14,12 +14,8 @@ describe('checkGuess()', () => {
         expect(result).toEqual('');
     });
 
-    it('does nothing with empty strings', () => {
-        const result = checkGuess('', 'WORDS');
-        expect(result).toEqual('');
-    });
 
-    it('outputs an array with objects for each correct, misplaced and incorrect letter', () => {
+    it('outputs an array with objects with only correct letter', () => {
         const result = checkGuess('BANAN', 'BANAN');
         expect(result).toEqual([
             { letter: 'B', result: 'correct' },
@@ -30,18 +26,7 @@ describe('checkGuess()', () => {
         ]);
     });
 
-    it('outputs an array with objects for each correct, misplaced and incorrect letter', () => {
-        const result = checkGuess('HALLÅ', 'HYVLA');
-        expect(result).toEqual([
-            { letter: 'H', result: 'correct' },
-            { letter: 'A', result: 'misplaced' },
-            { letter: 'L', result: 'incorrect' },
-            { letter: 'L', result: 'correct' },
-            { letter: 'Å', result: 'incorrect' }
-        ]);
-    });
-
-    it('outputs an array with objects for each correct, misplaced and incorrect letter', () => {
+    it('outputs an array with objects with only incorrect results', () => {
         const result = checkGuess('BANAN', 'ROLIG');
         expect(result).toEqual([
             { letter: 'B', result: 'incorrect' },
@@ -52,40 +37,38 @@ describe('checkGuess()', () => {
         ]);
     });
 
-    it('outputs an array with objects for each correct, misplaced and incorrect letter', () => {
+    it('outputs an array with objects with mixed results', () => {
+        const result = checkGuess('HALLÅ', 'HYVLA');
+        expect(result).toEqual([
+            { letter: 'H', result: 'correct' },
+            { letter: 'A', result: 'misplaced' },
+            { letter: 'L', result: 'incorrect' },
+            { letter: 'L', result: 'correct' },
+            { letter: 'Å', result: 'incorrect' }
+        ]);
+    });
+
+    it('outputs an array with objects with mixed results', () => {
         const result = checkGuess('TOMAT', 'TOMTE');
         expect(result).toEqual([
             { letter: 'T', result: 'correct' },
             { letter: 'O', result: 'correct' },
             { letter: 'M', result: 'correct' },
             { letter: 'A', result: 'incorrect' },
-            { letter: 'T', result: 'misplaced' },
+            { letter: 'T', result: 'misplaced' }
         ]);
     });
 
-    it('outputs an array with objects for each correct, misplaced and incorrect letter', () => {
-        const result = checkGuess('tOmaT', 'TOMTE');
+    it('tests inputs with mixed lowercase and uppercase letters', () => {
+        const result = checkGuess('tOmaT', 'TOMtE');
         expect(result).toEqual([
             { letter: 'T', result: 'correct' },
             { letter: 'O', result: 'correct' },
             { letter: 'M', result: 'correct' },
             { letter: 'A', result: 'incorrect' },
-            { letter: 'T', result: 'misplaced' },
+            { letter: 'T', result: 'misplaced' }
         ]);
     });
-
-    it('outputs an array with objects for each correct, misplaced and incorrect letter', () => {
-        const result = checkGuess('tOmaT', 'TOmtE');
-        expect(result).toEqual([
-            { letter: 'T', result: 'correct' },
-            { letter: 'O', result: 'correct' },
-            { letter: 'M', result: 'correct' },
-            { letter: 'A', result: 'incorrect' },
-            { letter: 'T', result: 'misplaced' },
-        ]);
-    });
-
-
 
 });
 
